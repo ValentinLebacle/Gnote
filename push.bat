1baf3bcdca39fbb9c387093da12289e6e5eab9ad
ValentinLebacle <valentin.lebacle@outlook.fr>
2025-05-02 17:37:07 +0200

    JFX | Ajout du formulaire modification de note

--- push.bat
+++ push.bat
@@ -6,7 +6,7 @@ git checkout -b Gnote_3.5
 
 git add .
 
-git commit -m "JFX | Ajout du formulaire de note"
+git commit -m "JFX | Ajout du formulaire modification de note"
 
 git push origin Gnote_3.5
 
