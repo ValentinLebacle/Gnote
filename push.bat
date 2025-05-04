d731a9c2525bc6e82c5941eb9a10ee6c28e9aeb9
ValentinLebacle <valentin.lebacle@outlook.fr>
2025-05-04 21:11:20 +0200

    JFX | Finalisation des tests unitaires

--- push.bat
+++ push.bat
@@ -6,7 +6,7 @@ git checkout -b Gnote_3.6
 
 git add .
 
-git commit -m "JFX | Finalisation de la page Info User"
+git commit -m "JFX | Finalisation des tests unitaires"
 
 git push origin Gnote_3.6
 
